a6668567503601b50ec43dcf883ffcbcc5d9b27a
thanhtan0308 <94156841+thanhtan0308@users.noreply.github.com>
2021-11-12 13:01:18 +0700

    Update config.php

--- admin/config/config.php
+++ admin/config/config.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli("localhost","root","","my_sqli");
+$mysqli = new mysqli("mysql5033.site4now.net","a7c5e7_myweb","thanhtan2001","a7c5e7_myweb");
 if ($mysqli->connect_errno)
 {
     echo"Kết nối MYSQL lỗi" . $mysqli->connect_error;
